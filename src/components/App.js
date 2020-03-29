bd5e69d419c684be0870288ca8b7db4b5d2b97c4
Steven Smith <steven.76484@gmail.com>
2020-03-29 08:38:29 -0400

    Adding CoursesPage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
 
 function App() {
   const route = window.location.pathname;
+  const getPage = route => {
+    switch (route) {
+      case "/about":
+        return <AboutPage />;
+      case "/courses":
+        return <CoursesPage />;
+      default:
+        return <HomePage />;
+    }
+  };
 
   return (
     <div className="container-fluid">
       <Header />
-      {route === "/about" ? <AboutPage /> : <HomePage />}
+      {getPage(route)}
     </div>
   );
 }
