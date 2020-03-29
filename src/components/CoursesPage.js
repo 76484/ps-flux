a5abe30b2ca33cde19516fdc74034d3ec2839dd8
Steven Smith <steven.76484@gmail.com>
2020-03-29 15:31:36 -0400

    Removing unwanted semi-colon

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -17,7 +17,7 @@ function CoursesPage() {
       <Link className="btn btn-primary" to="/course">
         Add Course
       </Link>
-      <CourseList courses={courses} />;
+      <CourseList courses={courses} />
     </>
   );
 }
