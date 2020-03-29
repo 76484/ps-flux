c7453a64350779adc402a1eab764574a5bda00eb
Steven Smith <steven.76484@gmail.com>
2020-03-29 12:32:48 -0400

    Adding Not Found Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route, Switch } from "react-router";
 
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
 import HomePage from "./HomePage";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/courses" component={CoursesPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
