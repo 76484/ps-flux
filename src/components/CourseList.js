c03c3291697bdc27872eb109778c2e98af6afd0a
Steven Smith <steven.76484@gmail.com>
2020-03-29 11:45:18 -0400

    Moving course list JSX into its own dumb component

---
+++ src/components/CourseList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function CourseList({ courses }) {
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default CourseList;
--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
@@ -9,31 +10,7 @@ function CoursesPage() {
     getCourses().then(setCourses);
   }, []);
 
-  return (
-    <>
-      <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author ID</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map(course => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
-  );
+  return <CourseList courses={courses} />;
 }
 
 export default CoursesPage;
